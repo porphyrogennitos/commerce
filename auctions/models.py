d5edde42f963381a79e0008a4c21a45ae681ef53
Porphyrogennitos <dionysisterzios@gmail.com>
2022-04-17 16:30:46 +0300

    Finish "Bid" model

--- auctions/models.py
+++ auctions/models.py
@@ -12,3 +12,9 @@ class Listing(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now=True)
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
